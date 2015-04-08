606c75fa34c38beecdf0704f69d81aadc5a955e7
user <user@appledeMacBook-Pro.local>
2015-04-08 11:38:11 +0800

    Initial Commit

---
+++ BlueTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlueTalk
+//
+//  Created by user on 15-4-8.
+//  Copyright (c) 2015年 YangPeiQiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlueTalk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueTalk
+//
+//  Created by user on 15-4-8.
+//  Copyright (c) 2015年 YangPeiQiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
